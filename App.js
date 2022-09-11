1695e570de0e1f0360683b1c3545b202fe5997fd
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-11 13:29:50 +0100

    app cronometro

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import {Containier,Titulo} from "./src/styles/Button"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Containier>
+        <Titulo>
+          Hello
+        </Titulo>
+    </Containier>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/Button.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Containier=styled.View`
+    flex: 1;
+    justify-content:center;
+    align-items: center;
+    background-color: #434343;
+
+`
+export const Titulo=styled.Text`
+color: #fff;
+font-size: 32px;
+font-weight: 700;
+font-family:fantasy;
+
+`
